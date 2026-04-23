eb5911550a93ab7bbfd74e0d39f4cee897897c81
lcontreras18 <123529844+lcontreras18@users.noreply.github.com>
2026-04-22 18:35:47 -0700

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>seen;
+        for(int i = 0; i < nums.size();i++){
+            int diff = target - nums[i];
+            if(seen.contains(diff)){
+                return {seen.at(diff),i};
+            }
+            seen.insert({nums[i],i});
+        }
+
+    }
+};
