a250a3fb375cc495e2d12a43390990decd3aef88
lcontreras18 <123529844+lcontreras18@users.noreply.github.com>
2026-07-16 16:12:06 -0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,46 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1Count = [0] * 26
+        s2Count = [0] * 26
+
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+        
+        matches = 0
+
+        for i in range(26):
+            if s1Count[i] == s2Count[i]:
+                matches += 1
+        
+        left = 0
+
+        for right in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            
+            rIndex = ord(s2[right]) - ord('a')
+            s2Count[rIndex] += 1
+
+            if s2Count[rIndex] == s1Count[rIndex]:
+                matches += 1 
+            elif s1Count[rIndex] + 1 == s2Count[rIndex]:
+                matches -= 1
+
+            leftIndex = ord(s2[left]) - ord('a')
+            s2Count[leftIndex] -= 1
+
+            if s2Count[leftIndex] == s1Count[leftIndex]:
+                matches += 1 
+            elif s1Count[leftIndex] - 1 == s2Count[leftIndex]:
+                matches -= 1
+
+            left +=1
+
+        return matches == 26
+
+            
+            
