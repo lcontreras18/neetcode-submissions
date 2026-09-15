9059f0381f3cae1bf15ef47bcb63f4ca7997daf2
lcontreras18 <123529844+lcontreras18@users.noreply.github.com>
2026-09-15 12:19:47 -0700

    Add: binary-search - submission-9

---
+++ Data Structures & Algorithms/binary-search/submission-9.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int middle = (right + left) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
+            }
+            
+            if (target > nums[middle]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+
+        }
+        return -1;
+    }
+};
